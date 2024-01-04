81bae5993b3c50be0042e30bfc5435101fb92857
Zubaer <105396721+ZubaerA23@users.noreply.github.com>
2024-01-03 19:45:43 -0500

    Add files via upload

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div className='container'>
+    <ul>
+      <li> Topic 1 <span> come this way </span></li>
+      <li> Topic 2 <span> come this way </span> </li>
+      <li> Topic 3 <span> come this way </span></li>
+    </ul>
+    </div>
+  )
+}
+
+export default Header
